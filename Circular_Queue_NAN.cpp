c11e67817f49d42c3a01dacb5fcff9427629e8fe
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-09-20 16:06:21 +0600

    Update Circular_Queue_NAN.cpp

--- Circular_Queue_NAN.cpp
+++ Circular_Queue_NAN.cpp
@@ -1,120 +1,172 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-int Queue[101]={0};
- int rear =0, front1 =0,range=7,item,option,number;
-
-void insert_Queue(int item){
+int Queue[101]= {0};
+int rear, front,range,item,option,number;
 
 
-     if(front1==0&&rear==0){
-        front1==1;
-         rear = 1;
-         Queue[rear]=item;
-             cout<<"          "<<item<<"         is insert successfully"<<endl;
-     return ;
-     }
-     else if((front1==1&&rear==range)||(front1==rear+1)){
-           cout<<"          "<<"            Overflow!!!!!"<<endl;
+void insert_Queue(int item)
+{
 
+    if((front==1&&rear==range)||(rear==range+1))
+    {
+        cout<<"Overflow??????"<<endl;
+        cout<<"Rear:"<<rear<<" && "<<"Front:"<<front<<endl;
+        return ;
+    }
+    else  if(front==0)
+    {
+        front=1;
+        rear =1;
+
+    }
+    else if(rear==range)
+    {
+        rear =1;
+    }
+    else
+    {
+        rear ++;
+    }
+
+
+    Queue[rear]=item;
+    cout<<item<<" is insert successfully"<<endl;
+    cout<<"Rear:"<<rear<<" && "<<"Front:"<<front<<endl;
     return;
-     }
 
-else if(rear==range){
-    rear =1;
-    Queue[rear] = item;
- cout<<"          "<<item<<"            is insert successfully"<<endl;
-   return ;
 }
-}
-int delete_Queue(){
+int delete_Queue()
+{
+    if(front==0)
+    {
 
-      if(front1==0) {
-        cout<<"          "<<"           Underflow!!!!!!!"<<endl;
-        return 0 ;
-      }
+        return -1;
 
-     else if(front1==range){
-        front1 = 1;
-        return item;
-      }
-     else if(front1==rear){
+    }
+
+
+    item = Queue[front];
+
+
+    if(front==rear)
+    {
+        front=0;
         rear =0;
-        front1 =0;
         return item;
-      }
-       item = Queue[front1];
-      front1=front1+1;
-      return item;
-}
+    }
+    else if(front==range)
+    {
+        front=1;
+        return item;
 
-void stop(){
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
-          cout<<"+++++++++++Thanks!!!!++++++++++"<<endl;
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
-          cout<<"+++++++++++Good Bye++++++++++++"<<endl;
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
-          cout<<"+++++Powered By : BSSE1129+++++"<<endl;
-          cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    }
+    else
+    {
+        front++;
+        return item;
+
+    }
+
 
 
 }
+void traverse_Queue()
+{
+    cout<<"|";
+    for(int iterator = front ; iterator<=rear ; iterator++)
+    {
+        iterator = iterator%range;
+
+        cout<<Queue[iterator]<<"|";
+
+    }
+    cout<<endl;
+
+}
+
+void stop()
+{
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++Thanks!!!!++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++Good Bye++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++Powered By : BSSE1129+++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++"<<endl;
 
 
+}
 
 
-int main(){
+int main()
+{
 
-cout<<"########=======================########"<<endl;
-cout<<"********.......................********"<<endl;
-cout<<"_______ Welcome to Circular Queue________"<<endl;
-cout<<"********.......................********"<<endl;
-cout<<"########=======================########"<<endl;
+    cout<<"########=======================########"<<endl;
+    cout<<"********.......................********"<<endl;
+    cout<<"_______ Welcome to Circular Queue________"<<endl;
+    cout<<"********.......................********"<<endl;
+    cout<<"########=======================########"<<endl;
 
-     int result;
+    int result;
+    rear=0,front=0;
 
 
-
-    cout<<"Enter the size of your Queue>>>>";
+    cout<<"Enter the size of your Queue>>>";
     cin>>range;
 
-   cout<<endl<<endl;
-     Menu_of_Introduction :
-   cout<<"What do you want ?"<<endl;
-   cout<<"1.Insert at Queue>>"<<endl;
-   cout<<"2.Delete at Queue>>"<<endl;
-   cout<<"3.Stop the program"<<endl;
-   cout<<endl<<endl;
-   cout<<"Press Your Key>>>>>>";
-   cin>>option;
+    cout<<endl<<endl;
+Menu_of_Introduction :
+    cout<<"What do you want ?"<<endl;
+    cout<<"1.Insert at Queue>>"<<endl;
+    cout<<"2.Delete at Queue>>"<<endl;
+    cout<<"3.Traverse at Queue>>"<<endl;
+    cout<<"4.Stop the program"<<endl;
+    cout<<endl<<endl;
+    cout<<"Press Your Key>>>>>>";
+    cin>>option;
 
 
-   if(option==1){
-       cout<<"Enter the number You want to insert>>>>>>";
-   cin>>number;
+    if(option==1)
+    {
+        cout<<"Enter the number You want to insert>>>";
+        cin>>number;
 
-   insert_Queue(number);
-   }
+        insert_Queue(number);
+    }
 
-   else if(option==2){
-   result = delete_Queue();
-   cout<<"         result"<<" is delete successfully!!!!!"<<endl;
-           }
+    else if(option==2)
+    {
+        int result = delete_Queue();
+        if(result==-1)
+        {
+            cout<<" Underflow!!!!!!!"<<endl;
+            cout<<"Rear:"<<rear<<" && "<<"Front:"<<front<<endl;
+        }
+        else
+        {
+            cout<<item<<" is delete successfully!!!!!"<<endl;
+            cout<<"Rear:"<<rear<<" && "<<"Front:"<<front<<endl;
+        }
+    }
+    else if(option==3)
+    {
+        traverse_Queue();
+    }
+    else if(option==4)
+    {
+        stop();
+        return 0 ;
+    }
 
-  else if(option==3){
-   stop();
-return 0 ;
-  }
-
-  else{
- goto Menu_of_Introduction ;
-   }
+    else
+    {
+        goto Menu_of_Introduction ;
+    }
 
 
 
-goto Menu_of_Introduction ;
-return 0 ;
+    goto Menu_of_Introduction ;
+    return 0 ;
 }
-
