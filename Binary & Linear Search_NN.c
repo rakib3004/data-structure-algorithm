2f5eb37a6dbde3af4127e81e89f4835c33440580
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-10-27 14:25:29 +0600

    Update Binary & Linear Search_NN.c

--- Binary & Linear Search_NN.c
+++ Binary & Linear Search_NN.c
@@ -1,69 +1,35 @@
-
 #include<stdio.h>
 
 int main()
 {
-
     int i;
-
     int n;
-
-
-
     printf("Input the size of your array>>>>");
-
     scanf("%d",&n);
-
-
-
     int a[n];
 
     for(i=0; i<n; i++)
     {
-
         printf("Item no %d >>>",i+1);
-
         scanf("%d",&a[i]);
-
-
     }
 
-
-
-    int f=0;
-
-
-
+   int f=0;
     for(i=0; i<n-1; i++)
     {
-
         if(a[i]<=a[i+1])
         {
             f=1;
-
         }
-
         else
         {
-
             f=0;
             break;
         }
-
     }
-
-
-
-
-
     printf("\n\nEnter Your Desired item>>>>>");
-
     int item;
-
-
     scanf("%d",&item);
-
-
     if(f==1)
     {
         printf("\n\n******Your Array is sorted****** \n");
@@ -74,12 +40,7 @@ int main()
     {
         printf("\n\n******Your Array is not  sorted****** \n");
         printf("\n\nNow Start Linear Search...............\n");
-
     }
-
-
-
-
 
     if(f==1)
     {
@@ -87,7 +48,6 @@ int main()
         int right_index=n-1;
 
         int mid=(left_index+right_index)/2;
-
         while(left_index<=right_index)
         {
 
@@ -112,47 +72,22 @@ int main()
             mid=(left_index+right_index)/2;
 
         }
-
-
         printf("\n\nThe item is not found in this array.!!!!!\n\n");
-
-
     }
 
     else
-
     {
-
-
-
-
         for(i=0; i<n; i++)
         {
             if(a[i]==item)
             {
-
-
                 printf("\n\nThe item is found!!!\n");
                 printf("\n\nThe item is at %d th index in this array.\n\n",i);
                 return 0;
             }
-
-
-
         }
-
         printf("\n\nThe item is not found in this array!!!!!\n\n");
 
     }
-
-
-
-
-
-
-
-
     return 0;
 }
-
-
