c4c3c954191e44aec59e4309b70b1c88b8ad9d3d
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-10-28 15:10:09 +0600

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -4,8 +4,38 @@ using namespace std;
 #define GRAY 1
 #define BLACK 2
 
+
 int G[100][100]/*={0}*/,color[100],d[100],prev[100],q[100];
 int front = 0 , rear =0;
+void  BFS(int s , int n){
+int u;
+    for(int i =0;i<n;i++){
+
+        color[i]=WHITE;
+        d[i]=99999;
+        prev[i]=-1;
+
+    }
+    color[s]=GRAY;
+    d[s]=0;
+    prev[s]=0;
+
+    q[rear++]=s;
+    while(front!=rear){
+        u=q[front++];
+        for(int v= 0;v<n;v++){
+            if(G[u][v]==1){
+                if(color[v]==WHITE){
+                    color[v]=GRAY;
+                    d[v]=d[u]+1;
+                    prev[v]=u;
+                    q[rear++]=v;
+
+                }
+            }
+        }
+    }
+}
 
 int main(){
 int n,e,s,u,v,i,j;
@@ -34,6 +64,25 @@ for(i=0;i<n;i++){
     }
     cout<<endl;
 }
+
+BFS(s,n);
+for(i=0;i<n;i++){
+        cout<<color[i]<<" ";
+}
+
+    cout<<endl;
+for(i=0;i<n;i++){
+        cout<<d[i]<<" ";
+}
+
+    cout<<endl;
+for(i=0;i<n;i++){
+        cout<<prev[i]<<" ";
+}
+
+    cout<<endl;
+
+
 return 0;
 
 }
